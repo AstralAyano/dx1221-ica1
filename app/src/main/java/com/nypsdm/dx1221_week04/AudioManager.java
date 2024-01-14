2ee8c0048eec34d1f1659f687d7a1c222f7a7152
Bernard Ng <bernardngboonhao@gmail.com>
2024-01-14 22:43:16 +0800

    Fixed AudioManager

--- app/src/main/java/com/nypsdm/dx1221_week04/AudioManager.java
+++ app/src/main/java/com/nypsdm/dx1221_week04/AudioManager.java
@@ -2,11 +2,13 @@ package com.nypsdm.dx1221_week04;
 
 import android.content.res.Resources;
 import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.view.SurfaceView;
 
 import java.util.HashMap;
 
-public class AudioManager {
+public class AudioManager
+{
     public final static AudioManager Instance = new AudioManager();
 
     private Resources res = null;
@@ -22,20 +24,44 @@ public class AudioManager {
     {
         view = _view;
         res = _view.getResources();
+        Release();
     }
 
-    public void PlayAudio(int _id, int left_vol)
+    private MediaPlayer GetAudio(int _id)
     {
         if (audioMap.containsKey(_id))
         {
-            audioMap.get(_id).reset();
-            audioMap.get(_id).start();
+            return audioMap.get(_id);
+        }
+
+        MediaPlayer result = MediaPlayer.create(view.getContext(), _id);
+        audioMap.put(_id, result);
+        return result;
+    }
+
+    public void PlayAudio(int _id, float _vol)
+    {
+        if (audioMap.containsKey(_id))
+        {
+            //audioMap.get(_id).reset();
+            //audioMap.get(_id).start();
+
+            MediaPlayer curr = audioMap.get(_id);
+            curr.seekTo(0);
+            curr.setVolume(_vol, _vol);
+            curr.start();
+        }
+        else
+        {
+            MediaPlayer curr = MediaPlayer.create(view.getContext(), _id);
+            audioMap.put(_id, curr);
+            curr.start();
         }
 
         // Load the audio
-        MediaPlayer newAudio = MediaPlayer.create(view.getContext(), _id);
+        /*MediaPlayer newAudio = MediaPlayer.create(view.getContext(), _id);
         audioMap.put(_id, newAudio);
-        newAudio.start();
+        newAudio.start();*/
     }
 
     public boolean IsPlaying(int _id)
@@ -44,5 +70,23 @@ public class AudioManager {
             return false;
 
         return audioMap.get(_id).isPlaying();
+    }
+
+    public void StopAudio(int _id)
+    {
+        MediaPlayer curr = audioMap.get(_id);
+        curr.stop();
+    }
+
+    public void Release()
+    {
+        for (HashMap.Entry<Integer, MediaPlayer> entry : audioMap.entrySet())
+        {
+            entry.getValue().stop();
+            entry.getValue().reset();
+            entry.getValue().release();
+        }
+
+        audioMap.clear();
     }
 }
